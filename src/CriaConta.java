d58c2e5f6aed4a38c0508f690492b020571b2fcf
Stefano Paulo <stefanopaulo47@gmail.com>
2021-04-13 18:22:29 -0300

    segunda instancia

--- src/CriaConta.java
+++ src/CriaConta.java
@@ -4,9 +4,17 @@ public class CriaConta {
 	public static void main(String[] args) {
 
 		Conta c1 = new Conta();
-		c1.saldo = 200;
 		
-		System.out.println(c1.saldo);
+		c1.saldo = 200;
+		System.out.println("Saldo da primeira conta: " + c1.saldo);
+		
+		c1.saldo += 100;
+		System.out.println("Saldo da primeira conta: " + c1.saldo);
+		
+		Conta c2 = new Conta();
+		
+		c2.saldo = 50;
+		System.out.println("Saldo da segunda conta: " + c2.saldo);
 		
 	}
 
