62d66829f00079c47ea8709a8287a4cd7604d356
Stefano Paulo <stefanopaulo47@gmail.com>
2021-04-13 19:35:15 -0300

    metodos com retorno

--- src/Conta.java
+++ src/Conta.java
@@ -5,5 +5,23 @@ public class Conta {
 	public int agencia;
 	public int numero;
 	public String titular;
+	
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+	
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void transfere(double valor, Conta conta) {
+		conta.saldo += valor;
+		this.saldo -= valor;
+	}
 
 }
---
+++ src/TestaMetodo.java
@@ -0,0 +1,25 @@
+
+public class TestaMetodo {
+	
+	public static void main(String[] args) {
+		
+		Conta conta = new Conta();
+		
+		conta.deposita(500);
+		
+		Conta conta2 = new Conta();
+		
+		conta.transfere(200, conta2);
+		
+		System.out.println("saldo da conta 1: " + conta.saldo);
+		System.out.println("saldo da conta 2: " + conta2.saldo);
+		
+		conta2.saca(500);
+		System.out.println("saldo da conta 2: " + conta2.saldo);
+		
+		boolean t = conta2.saca(50);
+		System.out.println("saldo da conta 2: " + conta2.saldo + " : " + t);
+		
+	}
+	
+}
