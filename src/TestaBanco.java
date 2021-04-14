1ae5f772693e287d1191b14073c6830b6704f3c2
Stefano Paulo <stefanopaulo47@gmail.com>
2021-04-14 19:17:17 -0300

    referencia null

--- src/TestaBanco.java
+++ src/TestaBanco.java
@@ -14,8 +14,17 @@ public class TestaBanco {
 		c1.titular = cl1;
 		c1.deposita(100);
 		
-		System.out.println("Titular da conta: " + c1.titular.nome);
-		System.out.println("Saldo em conta: " + c1.saldo);
+		// System.out.println("Titular da conta: " + c1.titular.nome);
+		// System.out.println("Saldo em conta: " + c1.saldo);
+		
+		Conta c2 = new Conta();
+		
+		System.out.println(c2.titular);
+		
+		c2.titular = new Cliente();
+		
+		c2.titular.nome = "Marcela";
+		System.out.println(c2.titular.nome);
 		
 	}
 
